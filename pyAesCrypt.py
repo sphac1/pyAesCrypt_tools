0f2bbf91d8abef3831b98c8c710e1a7ed6475090
sphac1 <valk.hac1@gmail.com>
2025-11-20 09:13:30 +0800

    Update pyAesCrypt.py

--- pyAesCrypt.py
+++ pyAesCrypt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-pyAesCrypt BruteForce Tool v2.2 (2024+ Compatible)
+pyAesCrypt BruteForce Tool  (2024+ Compatible)
 Multi-process AES decryption brute force tool with virtual environment auto-setup
 """
 
